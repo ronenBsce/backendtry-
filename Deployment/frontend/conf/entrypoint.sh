75e3fdd248b22b580e05f71e1ae8b7295704d6a6
ronenBsce <39045589+ronenBsce@users.noreply.github.com>
2023-04-04 10:45:49 +0000

    entry

--- Deployment/frontend/conf/entrypoint.sh
+++ Deployment/frontend/conf/entrypoint.sh
@@ -3,6 +3,6 @@
 set -eu
 
 echo "update Nginx config"
-envsubst '\${HOST1} \${HOST2} \${PORT}' < /etc/nginx/default.conf > /etc/nginx/nginx.conf
+envsubst '\${HOST1} \${HOST2} \${PORT}' < /usr/share/nginx/html/config/default.conf > /etc/nginx/nginx.conf
 
 exec "$@"
